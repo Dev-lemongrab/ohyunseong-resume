64c24d7e4082fd87dc6dd5f2760e2d6d11417dce
yunseongoh <gagizzang92@gmail.com>
2025-12-22 22:50:44 +0900

    fix : resume 원복

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,22 @@ import { Navbar, Footer } from "@/components";
 import Hero from "./hero";
 import InformationSection from "./information-section";
 import Testimonial from "./testimonial";
-import OrgTreemap from "@/components/orgTreemap";
-import OrgTreemap2 from "@/components/orgTreemap2";
+// import OrgTreemap from "@/components/orgTreemap";
+// import OrgTreemap2 from "@/components/orgTreemap2";
 
 export default function Portfolio() {
   return (
     <>
-    {/**
+
       <Navbar />
       <Testimonial />
       <Hero />
       <InformationSection />
       <Footer />
-       */}
+       {/**
         <OrgTreemap />
         <OrgTreemap2 />
+        */}
     </>
   );
 }
